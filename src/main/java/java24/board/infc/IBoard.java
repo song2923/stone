9756683fcf20c0db92cb8a11e0dbaf4879f668a3
love_sh_song <love_sh_song@hanmail.net>
2017-05-30 15:22:20 +0900

    test

---
+++ src/main/java/java24/board/infc/IBoard.java
@@ -0,0 +1,27 @@
+package java24.board.infc;
+
+import java.util.List;
+
+import java24.board.model.ModelArticle;
+
+public interface IBoard {
+    
+    //String getBoardName(String boardcd);
+    //ModelBoard getBoardOne(String boardcd);
+    //List<ModelBoard> getBoardList();
+    //List<ModelBoard> getBoardListResultMap();
+    
+    int getArticleTotalRecord(int boardcd);
+    
+    List<ModelArticle> getArticleList();
+    
+    ModelArticle getArticle(int articleNo);
+    
+    int insertArticle(ModelArticle TB_BBS_Article);
+    int updateArticle(ModelArticle updateValue, ModelArticle searchValue);
+    int deleteArticle(ModelArticle TB_BBS_Article);
+    
+    int increaseHit(int articleNo);
+    int getNextArticle();
+    
+}
