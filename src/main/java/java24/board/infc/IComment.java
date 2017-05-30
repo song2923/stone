7d9e48cad6a6ef271d11edd4db997a52179cdd3b
love_sh_song <love_sh_song@hanmail.net>
2017-05-30 16:55:45 +0900

    dd

--- src/main/java/java24/board/infc/IComment.java
+++ src/main/java/java24/board/infc/IComment.java
@@ -3,23 +3,24 @@ package java24.board.infc;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import java24.board.model.ModelComment;
+
+import java24.board.model.ModelComments;
 
 
 public interface IComment {
-    ResultSet SelectAll(ModelComment Cmm) throws SQLException;
+    ResultSet SelectAll(ModelComments cmm) throws SQLException;
 
-    ResultSet SelectLike( ModelComment Cmm) throws SQLException;
+    ResultSet SelectLike( ModelComments cmm) throws SQLException;
 
-    ResultSet SelectEqual( ModelComment Cmm) throws SQLException;
+    ResultSet SelectEqual( ModelComments cmm) throws SQLException;
 
-    int getCount(ModelComment Cmm) throws SQLException;
+    int getCount(ModelComments Cmm) throws SQLException;
 
     int getMaxAuthid() throws SQLException;
 
-    int Insert( ModelComment Cmm) throws SQLException;
+    int Insert( ModelComments Cmm) throws SQLException;
 
-    int Update( ModelComment whereCmm, ModelComment setCmm) throws SQLException;
+    int Update( ModelComments whereCmm, ModelComments setCmm) throws SQLException;
 
-    int Delete( ModelAuth auth) throws SQLException;
+    int Delete( ModelComments Cmm) throws SQLException;
 }
